83e3fad46239683f7604995a63cd0cf6ee154915
Dhanuka Warusadura <csx@tuta.io>
2025-03-19 22:51:50 +0530

    Update scan command description

--- src/cmd.c
+++ src/cmd.c
@@ -9,7 +9,7 @@ void print_help()
         printf("   \e[1mlist\e[0m                List external USB devices\n");
         printf("   \e[1mdump <id>\e[0m           Dump device information\n");
         printf("   \e[1mauthenticate <id>\e[0m   Check authenticity of a device\n");
-        printf("   \e[1mscan <id>\e[0m           Scan for malicious content and activities\n");
+        printf("   \e[1mscan <id>\e[0m           Scan for potential malicious factors\n");
         printf("   \e[1mhelp\e[0m                Print help\n");
         printf("   \e[1mversion\e[0m             Print version\n");
 }
